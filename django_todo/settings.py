52ce35c95d2b4231c5cb9137dadfece7d3e4ef6b
Aiden Harvey <aidenharvey102@gmail.com>
2019-05-20 15:38:11 +0000

    added template and rendered through Django

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # add todo app in order to run it
+    'todo'
 ]
 
 MIDDLEWARE = [
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 # All django view functions have to take a request as an argument
-# they handle the rrquest and always return HTTP response
-def say_hello(request):
-    return HttpResponse("Hello World")
+# they handle the request and always return HTTP response
 
+# render html template
+def get_todo_list(request):
+    return render(request, "todo_list.html")
