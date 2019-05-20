7c5bedb30e3b73fecdb5f98abb4e46171eb96f04
Aiden Harvey <aidenharvey102@gmail.com>
2019-05-20 16:17:38 +0000

    added basic model

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Item
 
 # Register your models here.
+# register database model "Item" in admin site
+admin.site.register(Item)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Item(models.Model):
+    # create a name field with a limit of 30 characters, not allowed to be empty
+    name = models.CharField(max_length=30, blank=False)
+    done = models.BooleanField(blank=False, default=False)
+    
+    def __str__(self):
+        return self.name
