b0ecebbc00d60788124b04412ea06ca287f27ac1
Aiden Harvey <aidenharvey102@gmail.com>
2019-05-31 12:47:35 +0000

    added tests for forms

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
+from .forms import ItemForm
 
 # Create your tests here.
-class TestDjango(TestCase):
+class TestToDoItemForm(TestCase):
     
     # tests must start with 'test_' or django won't detect them
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+    def test_can_create_an_item_with_just_name(self):
+        form = ItemForm({'name': 'Create tests'})
+        self.assertTrue(form.is_valid())
+    
+    def test_correct_message_for_missing_name(self):
+        form = ItemForm({'name': ''})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['name'], [u'This field is required.'])
